dc8a866c6a1a1106533581e39b4b4085900ffae8
Jordan <65262579+TheBigKahuna353@users.noreply.github.com>
2020-05-31 12:30:52 +1200

    added arrow and curve_rect shapes

--- hooman/shapes.py
+++ hooman/shapes.py
@@ -31,3 +31,31 @@ def alpha_ellipse(hapi, x, y, w, h):
     shape_fill = hapi._fill + (hapi._alpha,)
     pygame.draw.ellipse(surface1, shape_fill, (x, y, w, h))
     hapi.screen.blit(surface1, (0,0))
+
+def curve_rect(hapi, x, y, width, height, curve):
+    curve /= 200
+    curve = min(max(curve, 0), 1)
+    curve *= min(width, height)
+    curve = int(curve)
+    shape_fill = hapi._fill + (hapi._alpha,)
+    surf = pygame.Surface((width, height), pygame.SRCALPHA)
+    pygame.draw.rect(surf, shape_fill, (0, curve, width, height - 2 * curve))
+    pygame.draw.rect(surf, shape_fill, (curve, 0, width - 2 * curve, height))
+    pygame.draw.circle(surf, shape_fill, (curve, curve), curve)
+    pygame.draw.circle(surf, shape_fill, (width - curve, curve), curve)
+    pygame.draw.circle(surf, shape_fill, (curve, height - curve), curve)
+    pygame.draw.circle(surf, shape_fill, (width - curve, height - curve), curve)
+    hapi.screen.blit(surf,(x,y))
+
+def arrow(hapi, x, y, size, angle):
+    points = []
+    start = pygame.Vector2(x,y)
+    left = pygame.Vector2(-1,-2)
+    top = pygame.Vector2(0,-1)
+    right = pygame.Vector2(1,-2)
+    hapi.begin_shape()
+    hapi.vertex(start + left.rotate(angle) * size)
+    hapi.vertex(start + top.rotate(angle) * size)
+    hapi.vertex(start + right.rotate(angle) * size)
+    hapi.vertex(start)
+    hapi.end_shape()
