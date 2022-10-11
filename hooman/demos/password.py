ee5dd2f8135aa58e0b2d2a8589a2a5ed6a2b0a3c
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-10-11 09:00:58 +0400

    chore(demos): pattern password update

--- hooman/demos/password.py
+++ hooman/demos/password.py
@@ -1,16 +1,37 @@
 from hooman import Hooman
 from hooman.formula import distance
+from hooman.ui import Button
 
 import pygame
 
 pen = Hooman(500, 500)
 
-coords = []
-bids = []
-buttons = []
+attempt_button_coords = [] # coords for lock buttons in user pattern
+attempt_button_ids = [] # ids of lock buttons in user pattern
+buttons = [] # list of buttons to display
+display_win = False
+pattern = [1, 2, 3, 4, 5, 6, 7] # ids of password
 
 
-class B:
+# --- reset button ---
+def reset_clicked(this):
+    global attempt_button_coords, attempt_button_ids, display_win
+    attempt_button_coords = []
+    attempt_button_ids = []
+    display_win = False
+
+
+reset_button_styles = {
+    "hover_background_color": (200, 200, 200),
+    "font_size": 10,
+    "background_color": (210, 210, 210),
+    "on_click": reset_clicked,
+    "curve": 1,
+}
+reset_button = Button(250, 10, 150, 20, "Reset", reset_button_styles)
+
+
+class LockButton:
     def __init__(self, x, y, pen, num=0):
         self.x = x
         self.y = y
@@ -28,73 +49,80 @@ class B:
 
     def update(self):
         pen = self.pen
-        global coords, bids, just_ended
+        global attempt_button_coords, attempt_button_ids
 
         if pen.pygame.mouse.get_pressed()[0]:
 
-            if distance(self.coords(), (pen.mouseX(), pen.mouseY())) <= 20:
-                if self.coords() not in coords:
-                    if just_ended:
-                        just_ended = False
-                    else:
-                        bids.append(self.num)
-                        coords.append(self.coords())
+            mouse_coords = (pen.mouseX(), pen.mouseY())
+            if distance(self.coords(), mouse_coords) <= 20:
+                if self.coords() not in attempt_button_coords:
+                    attempt_button_ids.append(self.num)
+                    attempt_button_coords.append(self.coords())
 
     def run(self):
         self.update()
         self.draw()
 
 
-bid = 1
+# --- initialise buttons ---
+button_id = 1
 for x in range(5):
     for y in range(5):
 
-        buttons.append(B(x * 50 + 10, y * 50 + 10, pen, num=bid))
-        bid += 1
+        buttons.append(LockButton(x * 50 + 10, y * 50 + 10, pen, num=button_id))
+        button_id += 1
 
-display_win = False
-just_ended = False
 
 while pen.is_running:
     pen.background(255)
 
+    # --- draw lock pattern ---
     pen.fill(0)
-
-    for i, c in enumerate(coords):
+    for i, c in enumerate(attempt_button_coords):
         try:
             pen.stroke_size(5)
             pen.stroke(pen.color["green"])
-            pen.line(c[0] + 10, c[1] + 10, coords[i + 1][0] + 10, coords[i + 1][1] + 10)
+            pen.line(
+                c[0] + 10,
+                c[1] + 10,
+                attempt_button_coords[i + 1][0] + 10,
+                attempt_button_coords[i + 1][1] + 10,
+            )
         except Exception as e:
             pass
-            # print(coords, e)
 
+    # --- draw last part of pattern ---
     try:
-        pen.line(coords[-1][0] + 10, coords[-1][1] + 10, pen.mouseX(), pen.mouseY())
+        pen.line(
+            attempt_button_coords[-1][0] + 10,
+            attempt_button_coords[-1][1] + 10,
+            pen.mouseX(),
+            pen.mouseY(),
+        )
     except:
         pass
 
+    # --- display lock buttons ---
     for b in buttons:
         b.run()
 
-    if len(coords) >= 7:
-        try:
-            pen.stroke(pen.color["red"])
-            pen.line(coords[-1][0] + 10, coords[-1][1] + 10, pen.mouseX(), pen.mouseY())
-        except:
-            pass
 
-        if bids == [1, 2, 3, 4, 5, 6, 7]:
+    # --- if max pattern length ---
+    if len(attempt_button_coords) >= 7:
+
+        if attempt_button_ids == pattern:
             display_win = True
-            just_ended = True
-        coords = []
-        bids = []
 
+        attempt_button_coords = []
+        attempt_button_ids = []
+
+    # --- right password found ---
     if display_win:
-
         pen.fill((200, 200, 50))
         pen.font_size(30)
-        pen.text("Right password!", 300, 10)
+        pen.text("Right password!", 300, 50)
+
+    reset_button.update()
 
     pen.flip_display()
     pen.event_loop()
