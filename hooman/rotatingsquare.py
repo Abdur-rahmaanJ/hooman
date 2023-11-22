899f34336f82d80f2f8e166e99165eda205c0c3d
SahajaThota <tsahaja@g.clemson.edu>
2023-11-21 22:40:53 -0500

    new version

--- hooman/rotatingsquare.py
+++ hooman/rotatingsquare.py
@@ -1,4 +1,3 @@
-# demo_rotating_square.py
 from hooman import Hooman
 import pygame
 import math
@@ -13,12 +12,16 @@ bg_col = (255, 255, 255)
 square_size = 50
 square_x, square_y = window_width // 2, window_height // 2
 rotation_angle = 0
-rotation_speed = 2 
-square_color = (128, 0, 128)  
+rotation_speed = 2
+square_color = (128, 0, 128)
 
 # Trail
 trail_length = 10
 trail_positions = []
+
+# Set the desired frames per second (fps)
+fps = 60
+clock = pygame.time.Clock()
 
 def handle_events(event):
     if event.type == pygame.QUIT:
@@ -30,7 +33,6 @@ def handle_events(event):
 hapi.handle_events = handle_events
 
 while hapi.is_running:
-    
     hapi.background(bg_col)
 
     # Update square position and rotation
@@ -45,20 +47,23 @@ while hapi.is_running:
 
     # Draw trail
     for i, (x, y) in enumerate(trail_positions):
-        alpha = int(255 * (1 - i / trail_length)) 
-        hapi.fill(square_color[0], square_color[1], square_color[2])  
+        alpha = int(255 * (1 - i / trail_length))
+        hapi.fill(square_color[0], square_color[1], square_color[2])
         hapi.set_alpha(alpha)
         hapi.rect(x - square_size // 2, y - square_size // 2, square_size, square_size)
-        hapi.set_alpha(255) 
-    
+        hapi.set_alpha(255)
+
     # Draw rotating square
     hapi.rotate(rotation_angle)
     hapi.fill(square_color)
     hapi.rect(square_x - square_size // 2, square_y - square_size // 2, square_size, square_size)
-    hapi.rotate(-rotation_angle)  
+    hapi.rotate(-rotation_angle)
 
     # Update display and handle events
     hapi.flip_display()
     hapi.event_loop()
+
+    # Control frame rate
+    clock.tick(fps)
 
 pygame.quit()
