be3cf3731bb6caafaca446678fd15de70ed31524
chandana1221 <pothuguntachandana@gmail.com>
2023-11-22 23:42:33 -0500

    added calculatodemo.py

---
+++ hooman/demos/calculatordemo.py
@@ -0,0 +1,45 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Division by zero"
+
+def calculator():
+    print("Simple Calculator Demo")
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result}")
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print(f"{num1} - {num2} = {result}")
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print(f"{num1} * {num2} = {result}")
+    elif choice == '4':
+        result = divide(num1, num2)
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Invalid Input")
+
+if __name__ == "__main__":
+    calculator()
