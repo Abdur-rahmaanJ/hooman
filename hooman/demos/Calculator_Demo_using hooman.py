6a3a5db35768bae266b1643e99ae26803c67518a
BHARATH CHITTAMURU <148887430+bharathchittamuru7@users.noreply.github.com>
2023-11-24 23:45:30 -0500

    Update Calculator_Demo_using hooman.py
    updated Calculator_Demo_using hooman.py

--- hooman/demos/Calculator_Demo_using hooman.py
+++ hooman/demos/Calculator_Demo_using hooman.py
@@ -1,82 +1,84 @@
 from hooman import Hooman
 import pygame
-import ctypes
 
+class Calculator:
+    def _init_(self):
+        self.width, self.height = 600, 800
+        self.h = pygame.display.set_mode((self.width, self.height))
+        pygame.font.init()
+        self.font_big = pygame.font.Font(None, 50)
+        self.font_small = pygame.font.Font(None, 36)
+        self.expression = ""
+        self.result = ""
+        self.prev_left_button = False
 
-h = Hooman(600, 800)  
-pygame.font.init()
+        self.buttons = [
+            ("7", 20, 200, 120, 80), ("8", 160, 200, 120, 80), ("9", 300, 200, 120, 80), ("/", 440, 200, 120, 80),
+            ("4", 20, 300, 120, 80), ("5", 160, 300, 120, 80), ("6", 300, 300, 120, 80), ("*", 440, 300, 120, 80),
+            ("1", 20, 400, 120, 80), ("2", 160, 400, 120, 80), ("3", 300, 400, 120, 80), ("-", 440, 400, 120, 80),
+            ("C", 20, 500, 120, 80), ("0", 160, 500, 120, 80), ("=", 300, 500, 120, 80), 
+            ("+", 440, 500, 120, 80),
+        ]
 
+    def calculate(self):
+        try:
+            self.result = str(eval(self.expression))
+        except:
+            self.result = "Error"
 
-font_big = pygame.font.Font(None, 50)
-font_small = pygame.font.Font(None, 36)
+    def handle_button_click(self, button_text):
+        if button_text == "=":
+            self.calculate()
+        elif button_text == "C":
+            self.expression = ""
+            self.result = ""
+        else:
+            self.expression += button_text
 
-
-expression = ""
-result = ""
-
-
-h.prev_left_button = False
-
-
-def calculate():
-    try:
-        global result
-        result = str(eval(expression))
-    except:
-        result = "Error"
-
-
-def handle_button_click(button_text):
-    global expression, result
-    if button_text == "=":
-        calculate()
-    elif button_text == "C":
-        expression = ""
-        result = ""
-    else:
-        expression += button_text
-
-
-while h.is_running:
-    h.background(h.color["grey"])  
-
-    
-    text_surface = font_big.render(expression, True, pygame.Color("white"))
-    h.screen.blit(text_surface, (20, 50))
-
-    
-    text_surface = font_big.render(result, True, pygame.Color("black"))
-    h.screen.blit(text_surface, (20, 100))
-
-    
-    buttons = [
-        ("7", 20, 200, 120, 80), ("8", 160, 200, 120, 80), ("9", 300, 200, 120, 80), ("/", 440, 200, 120, 80),
-        ("4", 20, 300, 120, 80), ("5", 160, 300, 120, 80), ("6", 300, 300, 120, 80), ("*", 440, 300, 120, 80),
-        ("1", 20, 400, 120, 80), ("2", 160, 400, 120, 80), ("3", 300, 400, 120, 80), ("-", 440, 400, 120, 80),
-        ("C", 20, 500, 120, 80), ("0", 160, 500, 120, 80), ("=", 300, 500, 120, 80),  # Removed the "." button
-        ("+", 440, 500, 120, 80),
-    ]
-
-    for button in buttons:
-        text, x, y, width, height = button
-        pygame.draw.rect(h.screen, pygame.Color(200, 200, 200), (x, y, width, height))
-        
-        text_surface = font_small.render(text, True, pygame.Color("blue"))
+    def draw_button(self, text, x, y, width, height):
+        pygame.draw.rect(self.h, pygame.Color(200, 200, 200), (x, y, width, height))
+        text_surface = self.font_small.render(text, True, pygame.Color("blue"))
         text_rect = text_surface.get_rect(center=(x + width // 2, y + height // 2))
-        h.screen.blit(text_surface, text_rect)
-        
-   
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    left_button, _, _ = pygame.mouse.get_pressed()
+        self.h.blit(text_surface, text_rect)
 
-    if left_button and not h.prev_left_button:
-        for button in buttons:
+    def draw(self):
+        self.h.fill(pygame.Color("grey"))
+
+        text_surface = self.font_big.render(self.expression, True, pygame.Color("white"))
+        self.h.blit(text_surface, (20, 50))
+
+        text_surface = self.font_big.render(self.result, True, pygame.Color("black"))
+        self.h.blit(text_surface, (20, 100))
+
+        for button in self.buttons:
             text, x, y, width, height = button
-            if x < mouse_x < x + width and y < mouse_y < y + height:
-                handle_button_click(text)
+            self.draw_button(text, x, y, width, height)
 
-   
-    h.prev_left_button = left_button
+        pygame.display.flip()
 
-    h.flip_display()
-    h.event_loop()
+    def event_loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
+
+    def run(self):
+        self.is_running = True
+        while self.is_running:
+            self.draw()
+
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            left_button, _, _ = pygame.mouse.get_pressed()
+
+            if left_button and not self.prev_left_button:
+                for button in self.buttons:
+                    text, x, y, width, height = button
+                    if x < mouse_x < x + width and y < mouse_y < y + height:
+                        self.handle_button_click(text)
+
+            self.prev_left_button = left_button
+
+            self.event_loop()
+
+if _name_ == "_main_":
+    calculator = Calculator()
+    calculator.run()
